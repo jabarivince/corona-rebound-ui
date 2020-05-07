7874fd9a8b4d0dc7483ade5c8035458f6ae70a0f
Jabari Dash <jabari.dash@gmail.com>
2020-05-07 01:01:53 -0500

    added ads

---
+++ corona-rebound-ui/public/index.js
@@ -0,0 +1,42 @@
+function setUpFirebase() {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBJgE1tubVU5ud0AQUGN4e2bRAVBO_k43c",
+    authDomain: "corona-rebound.firebaseapp.com",
+    databaseURL: "https://corona-rebound.firebaseio.com",
+    projectId: "corona-rebound",
+    storageBucket: "corona-rebound.appspot.com",
+    messagingSenderId: "1077806466597",
+    appId: "1:1077806466597:web:b9a3d6eef2d99816c6a1cc",
+    measurementId: "G-YWYFVNKYKH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
+
+function setupTwitter() {
+  !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+  },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+  a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+  // Insert Twitter Pixel ID and Standard Event data below
+  twq('init','o3oq8');
+  twq('track','PageView');
+}
+
+function handleAds() {
+  window.addEventListener('load', function () {
+    let iframes = document.getElementsByTagName('iframe')
+
+    let topBanner = iframes[0]
+    let bottomBanner = iframes[1]
+
+    if (screen.width < topBanner.width) {
+      topBanner.width = screen.width
+      topBanner.src = topBanner.src
+
+      bottomBanner.width = screen.width
+      bottomBanner.src = bottomBanner.src
+    }
+  })
+}
